d157224f49d6bfac60f39e01bd681fb25f628def
Ben Meadors <benmmeadors@gmail.com>
2024-09-09 19:57:31 -0500

    Example

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,28 @@
 #if MESHTASTIC_I2C_SENSOR_HOST
 #include <I2CHost.h>
 
+meshtastic_EnvironmentMetrics metrics = meshtastic_EnvironmentMetrics_init_zero;
+
+void onReceiveRequest(int numBytes) {
+    sendMetrics(metrics);
+}
+
 void setup() {
-    while (!Serial) { delay(10); }
     Serial.begin(115200);
     Serial.println("Initializing Meshtastic I2C Sensor device (master)...");
 
-    Wire.begin();
-    Wire.write(reinterpret_cast<uint8_t*>(MT_I2C_VERSION), 1);
+    Wire.begin(MT_I2C_ADDRESS);
+    Wire.beginTransmission(MT_I2C_ADDRESS);
+    Wire.write(0x69);
+    Wire.endTransmission();
+    Wire.onReceive(onReceiveRequest);
 }
 
 void loop() {
-    meshtastic_EnvironmentMetrics metrics = meshtastic_EnvironmentMetrics_init_zero;
     metrics.has_temperature = true;
     metrics.temperature = 25.0;
+    metrics.has_relative_humidity = true;
+    metrics.relative_humidity = 50.0;
     sendMetrics(metrics);
     delay(100);
 }
@@ -24,7 +33,6 @@ void loop() {
 #include <I2CClient.h>
 
 void setup() {
-    while (!Serial) { delay(10); }
     Serial.begin(115200);
     Serial.println("Initializing Meshtastic I2C Sensor device (client)...");
     Wire.begin(MT_I2C_ADDRESS);
